017e3a7d6f2e482132f9844fc6b885df333be5b1
Miguel Angel López <admon@tecnott.com.mx>
2023-03-13 14:26:51 -0600

    Added challege of use of Spotify API with spotipy library

--- restart_playlist.py
+++ restart_playlist.py
@@ -1,23 +1,64 @@
-import os
-import sys
-import json
 import spotipy
-import webbrowser
-import spotipy.util as util
-from json.decoder import JSONDecodeError
+from spotipy.oauth2 import SpotifyOAuth
 
-# Obtiene el username desde la terminal
-username = sys.argv[1]
 
-# Borra el cache y pide permiso al usuario
-try:
-    token = util.prompt_for_user_token(username)
-except:
-    os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username)
+# Convierte los segundos a un formato de HH:mm:ss
+def convert_time(seconds):
+    hours = int(seconds / 3600)
+    seconds -= hours * 3600
+    minutes = int(seconds / 60)
+    seconds -= minutes * 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+
+def user_credentials():
+    scope = "playlist-modify-private"
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+
+def find_artist(search_query):
+    # Obtiene los resultados de la busqueda
+    search_results = spotify_object.search(search_query, 1, 0, "artist")
+
+    # Detalles del artista
+    artist = search_results["artists"]["items"][0]
+    print(f"Artista:  {artist['name']}")
+    print(f"Seguidores: {artist['followers']['total']}")
+    print(f"Genero: {artist['genres'][0]}")
+    artist_ID = artist["id"]
+    return artist_ID
+
+
+def find_top_10_tracks(artist_ID):
+    artist_top_10_tracks = spotify_object.artist_top_tracks(artist_ID, "MX")
+    i = 0
+    for track in artist_top_10_tracks["tracks"]:
+        track_number = i + 1
+        name = track["name"]
+        popularity = track["popularity"]
+        duration = track["duration_ms"]
+        track_ID = track["id"]
+
+        print(f" {track_number} - {name[:50]} - {popularity} ")
+        i += 1
+
+    return artist_top_10_tracks
+
+
+# def create_playlist(user_id, list_track_id, playlist_name):
+#     # Crea la playlist en Spotify
+#     playlist_id = spotify_object.user_playlist_create(
+#         user_id, playlist_name, False, False
+#     )
+#     list_track_id = ",".join(str(track) for track in list_track_id)
+#     print(playlist_id["id"])
+#     print(list_track_id)
+#     # Agrega los tracks a la playlist recien creada
+#     spotify_object.playlist_add_items(playlist_id["id"], list_track_id)
+
 
 # Creamos nuestro objeto spotify
-spotify_object = spotipy.Spotify(auth=token)
+spotify_object = user_credentials()
 
 # print(json.dumps(VARIABLE, sort_keys=True, indent=4))
 
@@ -26,75 +67,51 @@ user = spotify_object.current_user()
 
 display_name = user["display_name"]
 followers = user["followers"]["total"]
+user_ID = user["id"]
 
 while True:
     print()
-    print(f">>> Bienvenido a Restart Playlist {display_name} !")
-    print(f">>> Tu tienes {str(followers)} seguidores")
+    print(f">>> Bienvenido a Restart Playlist {display_name}! <<<")
+    print(f">>> Tu tienes {str(followers)} seguidores <<<")
     print()
     print("0 - Busqueda por un artista")
-    print("1 - Busqueda por canción")
-    print("2 - Salir")
+    print("1 - Salir")
     print()
-    choice = input("Tu opción: ")
+    choice = input("Selecciona tu opción: ")
 
     # Busca por un artista
     if choice == "0":
         print()
         search_query = input("¿Cúal es su nombre?: ")
         print()
-
-        # Obtiene los resultados de la busqueda
-        search_results = spotify_object.search(search_query, 1, 0, "artist")
-
-        # Detalles del artista
-        artist = search_results["artists"]["items"][0]
-        print(artist["name"])
-        print(f"{str(artist['followers']['total'])} seguidores")
-        print(artist["genres"][0])
-        artist_ID = artist["id"]
-        print()
-
+        artist_ID = find_artist(search_query)
+        print("*" * 30, end="TOP 10" + ("*" * 44) + "\n")
         # Obtiene el top 10 del artista capturado
-        artist_top_10_tracks = spotify_object.artist_top_tracks(artist_ID, "MX")
-
-        # print(json.dumps(artist_top_10_tracks, sort_keys=True, indent=4))
+        artist_top_10_tracks = find_top_10_tracks(artist_ID)
         print()
-        i = 0
-        for track in artist_top_10_tracks["tracks"]:
-            track_number = i + 1
-            name = track["name"]
-            popularity = track["popularity"]
-            duration = track["duration_ms"]
-            track_ID = track["id"]
-
-            print(f" {track_number} - {name} - {popularity} ")
-            i += 1
-
         print("*" * 80)
         print("Crear playlist")
 
-        # Pide número de canción para agregarla a un diccionario
-        # Declaración de variables
-        playlist_dict = {}
-        list_playlist_dict = []
+        # Pide número de canción para agregar sus datos en listas
+        # Declaración de listas
         list_track_ID = []
         list_track_name = []
         list_track_duration = []
+
         while True:
             song_selection = input("Captura el número de una canción (x para salir): ")
             if song_selection == "x":
                 break
 
             # Almacena la información de la canción seleccionada en listas
-            store_track_ID = artist_top_10_tracks["tracks"][
-                int(song_selection) - 1
-            ]["id"]
+            store_track_ID = artist_top_10_tracks["tracks"][int(song_selection) - 1][
+                "id"
+            ]
             list_track_ID.append(store_track_ID)
 
-            store_track_name = artist_top_10_tracks["tracks"][
-                int(song_selection) - 1
-            ]["name"]
+            store_track_name = artist_top_10_tracks["tracks"][int(song_selection) - 1][
+                "name"
+            ]
             list_track_name.append(store_track_name)
 
             store_track_duration = artist_top_10_tracks["tracks"][
@@ -105,50 +122,26 @@ while True:
             print("*" * 80)
             print("La canción que seleccionaste fue:")
             print(f"Canción: {store_track_name}")
-            print(f"Duración: {store_track_duration/60000}")
+            print(f"Duración: {convert_time(int(store_track_duration/1000))}")
 
-        print(list_track_ID)
-        print(list_track_name)
-        print(list_track_duration)
+        if len(list_track_ID) >= 2:
+            playlist_duration = sum(list_track_duration)
+            print(
+                f"El tiempo total de tu playlist es: {convert_time(int(playlist_duration/1000))}"
+            )
+            # while True:
+            #     option = input("Desea crear la playlist S/N: ?")
+            #     if option.upper() == "S":
+            #         playlist_name = input("Nombre de la playlist: ")
+            #         create_playlist(user_ID, list_track_ID, playlist_name)
+            #         print("Playlist creada satisfactoriamente!!!")
+            #         break
 
-
-        # # Detalles del Album y pistas
-        # track_URIs = []
-        # track_art = []
-        # i = 0
-
-        # # Extrae datos del Album
-        # album_results = spotify_object.artist_albums(artist_ID)
-        # album_results = album_results["items"]
-        # for item in album_results:
-        #     print(f"ALBUM {item['name']}")
-        #     album_ID = item["id"]
-        #     album_art = item["images"][0]["url"]
-
-        # # Extrae datos de la pista
-        # track_results = spotify_object.album_tracks(album_ID)
-        # track_results = track_results["items"]
-
-        # for item in track_results:
-        #     print(f"{str(i)} : {item['name']}")
-        #     track_URIs.append(item["uri"])
-        #     track_art.append(album_art)
-        #     i += 1
-        # print()
-
-        # # Ver arte del Album
-        # while True:
-        #     song_selection = input(
-        #         "Captura eel número de una canción para ver el arte del album asociado (x para salir): "
-        #     )
-        #     if song_selection == "x":
-        #         break
-        #     webbrowser.open(track_art[int(song_selection)])
-
-    # Busqueda por canción
-    if choice == "1":
-        pass
+            #     elif option.upper() == "N":
+            #         break
+            #     else:
+            #         print("Opción invalida")
 
     # Fin del programa
-    if choice == "2":
+    if choice == "1":
         break
